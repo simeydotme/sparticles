c180693b0df9016ff944bdbaca6a82cd5c1b0b84
simeydotme <simey.me@gmail.com>
2020-03-10 02:50:41 +0800

    add a sveltejs example folder

---
+++ example/svelte/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Sparticles from "../../../dist/sparticles.esm.js";
+  let sparticles;
+  let options = { color: "gold", shape: "star", speed: 50 };
+  function addSparticles(node) {
+    new Sparticles(node, options);
+  }
+</script>
+
+<main use:addSparticles>
+</main>
+
+<style>
+  main {
+    height: 100%;
+  }
+</style>
